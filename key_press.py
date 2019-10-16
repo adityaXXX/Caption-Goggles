0e82fed793e7ebd03b7d84ac6cc1c6ed85b5f855
Deepak Hirawat <bit.deepak99@gmail.com>
2019-10-16 21:52:12 +0530

    Key press detection addded

---
+++ key_press.py
@@ -0,0 +1,24 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        print(key.char == 'a')
+    except:
+        pass
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-import keyboard
+from pynput import keyboard
 from flask import Flask, render_template, Response
 import cv2
 from captionbot import CaptionBot
@@ -11,8 +11,29 @@ def index():
     user = {'username':"Deepak"}
     return render_template('index.html' , user= user)
 
+def on_press(key):
+    try:
+        if key.char == 'a':
+            print(key.char == 'a')
+            print("Generating Caption...")
+            cv2.imwrite('image.jpg',f)
+            caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
+            # caption = c.file_caption("C:/Users/Bharat/Desktop/Caption-Goggles/image.jpg")
+            print(caption)
+    except:
+        pass
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
 def gen():
     cap = cv2.VideoCapture(0)
+    with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+        listener.join()
+    listener = keyboard.Listener(on_press=on_press,on_release=on_release)
+    listener.start()
     while True:
         ret ,frame =  cap.read()
         if ret == True:
@@ -21,19 +42,6 @@ def gen():
                 continue
             yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
-            while True:
-                try:
-                    if keyboard.is_pressed('a'):
-                        # if key == ord('a'):
-                        print("Generating Caption...")
-                        cv2.imwrite('image.jpg',f)
-                        caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
-                        # caption = c.file_caption("C:/Users/Bharat/Desktop/Caption-Goggles/image.jpg")
-                        print(caption)
-                    else:
-                        pass
-                except:
-                    break
         else:
             continue
     cv2.destroyAllWindows()
