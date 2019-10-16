6e585c689ed01137bfdc2a06db1c1743cfdf5932
Deepak Hirawat <bit.deepak99@gmail.com>
2019-10-16 19:52:22 +0530

    added try else in caption.py

--- caption.py
+++ caption.py
@@ -5,14 +5,18 @@ c = CaptionBot()
 cap = cv2.VideoCapture(0)
 while True:
     r, f = cap.read()
-    cv2.imshow("Stream", f)
-    key = cv2.waitKey(1)
-    if key == ord('a'):
-        print("Generating Caption...")
-        cv2.imwrite('image.jpg',f)
-        caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
-        print(caption)
-    elif key == 27:
-        break
+    if r==True:
+        cv2.imshow("Stream", f)
+        key = cv2.waitKey(1)
+        if key == ord('a'):
+            print("Generating Caption...")
+            cv2.imwrite('image.jpg',f)
+            # caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
+            caption = c.file_caption("C:/Users/Bharat/Desktop/Caption-Goggles/image.jpg")
+            print(caption)
+        elif key == 27:
+            break
+    else :
+        continue
 cv2.destroyAllWindows()
 cap.release()
