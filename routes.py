3d881a01224a701b1f2258befee90dbb326065df
Deepak Hirawat <bit.deepak99@gmail.com>
2019-10-16 19:50:03 +0530

    Flask Server added

---
+++ routes.py
@@ -0,0 +1,41 @@
+# from app import app
+from flask import Flask, render_template, Response
+import cv2
+from captionbot import CaptionBot
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username':"Deepak"}
+    return render_template('index.html' , user= user)
+
+def gen():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret ,frame =  cap.read()
+        if ret == True:
+            yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            key = cv2.waitKey(1)
+            if key == ord('a'):
+                print("Generating Caption...")
+                cv2.imwrite('image.jpg',f)
+                caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
+                # caption = c.file_caption("C:/Users/Bharat/Desktop/Caption-Goggles/image.jpg")
+                print(caption)
+            elif key == 27:
+                break
+        else:
+            continue
+    cv2.destroyAllWindows()
+    cap.release()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
