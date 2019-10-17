e8d96ce107dbf52979133dc547d26a2c8ad0b3e8
Deepak Hirawat <bit.deepak99@gmail.com>
2019-10-17 19:19:24 +0530

    Added Caption Button

--- routes.py
+++ routes.py
@@ -2,8 +2,11 @@ from pynput import keyboard
 from flask import Flask, render_template, Response
 import cv2
 from captionbot import CaptionBot
+import numpy as np
 
 app = Flask(__name__)
+
+frame = None
 
 @app.route('/')
 @app.route('/index')
@@ -17,24 +20,19 @@ def on_press(key):
             print(key.char == 'a')
             print("Generating Caption...")
             cv2.imwrite('image.jpg',f)
-            caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
-            # caption = c.file_caption("C:/Users/Bharat/Desktop/Caption-Goggles/image.jpg")
+            # caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
+            print("lol")
+            caption = c.file_caption("C:/Users/Bharat/Desktop/Caption-Goggles/image.jpg")
+            print("yeah!!")
             print(caption)
+            print("hahah")
     except:
         pass
 
-def on_release(key):
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
 def gen():
     cap = cv2.VideoCapture(0)
-    with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
-        listener.join()
-    listener = keyboard.Listener(on_press=on_press,on_release=on_release)
-    listener.start()
     while True:
+        global frame
         ret ,frame =  cap.read()
         if ret == True:
             flag, encodedImage = cv2.imencode(".jpg" , frame)
@@ -43,14 +41,28 @@ def gen():
             yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
         else:
+            # with keyboard.Listener(on_press=on_press) as listener:
+            #     listener.join()
+            #     listener = keyboard.Listener(on_press=on_press)
+            #     listener.start()
             continue
-    cv2.destroyAllWindows()
     cap.release()
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/generate_caption')
+def generate_caption():
+    c = CaptionBot()
+    print("Generating Caption...")
+    global frame
+    cv2.imwrite('image.jpg' , frame)
+    # caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
+    caption = c.file_caption("C:/Users/Bharat/Desktop/Caption-Goggles/image.jpg")
+    print(caption)
+    return caption
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
